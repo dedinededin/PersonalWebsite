c5af6fa537c497f462af16595a54fd565026c779
dedinededin <41920662+dedinededin@users.noreply.github.com>
2018-12-03 01:12:04 +0300

    ready to deploy

--- mysite/views.py
+++ mysite/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from .models import Contact
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'mysite/index.html', {'nbar':'home'})
+    context = {'home': 'active'}
+    return render(request, 'mysite/index.html', context)
+
 
 def portfolio(request):
-    return render(request, 'mysite/portfolio.html', {'nbar':'portfolio'})
+    context = {'portfolio': 'active'}
+    return render(request, 'mysite/portfolio.html', context)
+
 
 def contact(request):
-    return render(request, 'mysite/contact.html', {'nbar':'contact'})
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        c = Contact(email=email, subject=subject, message=message)
+        c.save()
+        context = {'contact': 'active', 'message': 'We got your mail'}
+        return render(request, 'mysite/contact.html', context)
+    else:
+        context = {'contact': 'active'}
+        return render(request, 'mysite/contact.html', context)
